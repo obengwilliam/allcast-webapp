8855b93c06079c9488f12677983c2242e7cd168f
obengwilliam <obeng.kojo.william@gmail.com>
2014-11-18 10:26:56 +0000

    refactor(server.js): remove specifiend dev log level

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express=require('express');
 var app=express();
 
 
-app.use(express.logger('dev'));
+app.use(express.logger());
 app.use(express.static(__dirname + '/build'));
 app.listen(process.env.PORT || 5000);
 
