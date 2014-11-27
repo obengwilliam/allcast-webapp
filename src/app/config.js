254dd10025f6a2bdb60268cf1903aa4f1c14e73d
obengwilliam <obeng.kojo.william@gmail.com>
2014-11-27 09:51:07 +0000

    stage

--- src/app/config.js
+++ src/app/config.js
@@ -3,8 +3,8 @@
  angular.module("allcast.config", [])
 
 .constant("ENV", {
-  "name": "development",
-  "API_SERVER": "http://localhost:3000/api/v1.0/"
+  "name": "staging",
+  "API_SERVER": "http://allcast-signal-server-stage.herokuapp.com/api/v1.0/"
 })
 
 ;
